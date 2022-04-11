55850e109066a3d48bca9957d5a17f9a01dfef26
mrkouhadi <teacherbryan@outlook.com>
2022-04-12 05:38:05 +0800

    adding authentication when logging

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,7 +1,19 @@
+import { useState } from 'react';
 import AuthContent from '../components/Auth/AuthContent';
+import LoadingOverlay from '../components/ui/LoadingOverlay';
+import { login } from '../util/auth';
 
 function LoginScreen() {
-  return <AuthContent isLogin />;
+  const [isLogin, setIsLogin] = useState(false);
+
+  const loginHandler = async ({email,password})=>{
+    setIsLogin(true);
+    await login(email,password);
+    setIsLogin(false);
+  }
+  if(isLogin) return <LoadingOverlay  message="Authenticating..." / >
+
+  return <AuthContent isLogin onAuthenticate={loginHandler} />;
 }
 
 export default LoginScreen;
