913be5a30d4fd1e53e39a4eaf1978d5caf055c76
mrkouhadi <teacherbryan@outlook.com>
2022-04-12 05:18:14 +0800

    .

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,11 +1,16 @@
+import { useState } from 'react';
 import AuthContent from '../components/Auth/AuthContent';
 import { createUser } from '../util/auth';
-
+import LoadingOverlay from '../components/ui/LoadingOverlay';
 function SignupScreen() {
+  const [isRegistering, setIsRegistering] = useState(false);
 
-  const signupHandler = ()=>{
-    createUser();
+  const signupHandler = async ({email,password})=>{
+    setIsRegistering(true);
+    await createUser(email,password);
+    setIsRegistering(false);
   }
+  if(isRegistering) return <LoadingOverlay message="Creating a New User..." / >
   return <AuthContent onAuthenticate={signupHandler} />;
 }
 
