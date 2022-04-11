781d28a88bc8bcca17ab3ca748bf234cc8824b16
mrkouhadi <teacherbryan@outlook.com>
2022-04-12 04:56:59 +0800

    install axios

---
+++ util/auth.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const API_KEY = "AIzaSyAlOjfCkrGF8NBbtr2xSGuEexUpWkSrMcw"
+const BACKEND_URL = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`;
+
+
+const createUser = async (email, password)=>{
+    const response = await axios.post(BACKEND_URL, {
+                        email, 
+                        password, 
+                        returnSecureToken:true
+                    });
+    
+}
