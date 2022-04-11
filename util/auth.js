c410833ab456e49e5f43e2a422d9286cfc0c6642
mrkouhadi <teacherbryan@outlook.com>
2022-04-12 05:29:41 +0800

    add lgogin function

--- util/auth.js
+++ util/auth.js
@@ -1,14 +1,20 @@
 import axios from "axios";
 
 const API_KEY = "AIzaSyAlOjfCkrGF8NBbtr2xSGuEexUpWkSrMcw"
-const BACKEND_URL = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`;
 
-
-export const createUser = async (email, password)=>{
-     await axios.post(BACKEND_URL, {
+ const authenticate = async(mode, email, password)=>{
+    const BACKEND_URL = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
+    
+    const response = await axios.post(BACKEND_URL, {
                         email, 
                         password, 
                         returnSecureToken:true
                     });
-                    // const response =
+}
+export const createUser = async (email, password) =>{
+    await authenticate('signUp', email, password);
+}
+
+export const login = async (email, password) =>{
+    await authenticate('signInWithPassword', email, password);
 }
