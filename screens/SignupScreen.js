df41280b07e4fcfe6e63b71c8577f43ce930bb33
mrkouhadi <teacherbryan@outlook.com>
2022-04-12 05:02:39 +0800

    adding signup handler and triggering createUser function

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,7 +1,12 @@
 import AuthContent from '../components/Auth/AuthContent';
+import { createUser } from '../util/auth';
 
 function SignupScreen() {
-  return <AuthContent />;
+
+  const signupHandler = ()=>{
+    createUser();
+  }
+  return <AuthContent onAuthenticate={signupHandler} />;
 }
 
 export default SignupScreen;
