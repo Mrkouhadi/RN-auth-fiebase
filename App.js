5713c1178201278588e521febb56751bf867c3fb
mrkouhadi <teacherbryan@outlook.com>
2022-04-13 01:25:21 +0800

    securing the navigator and showing welcome screen when is authecated

--- App.js
+++ App.js
@@ -6,7 +6,8 @@ import LoginScreen from './screens/LoginScreen';
 import SignupScreen from './screens/SignupScreen';
 import WelcomeScreen from './screens/WelcomeScreen';
 import { Colors } from './constants/styles';
-import AuthContextProvider from './store/auth-context';
+import AuthContextProvider, { AuthContext } from './store/auth-context';
+import { useContext } from 'react';
 
 const Stack = createNativeStackNavigator();
 
@@ -40,10 +41,14 @@ function AuthenticatedStack() {
 }
 
 function Navigation() {
+  const authCtx = useContext(AuthContext)
+
   return (
     <AuthContextProvider>
       <NavigationContainer>
-        <AuthStack />
+        {
+          !authCtx.isAuthenticated ? <AuthenticatedStack /> :  <AuthStack />
+        }
       </NavigationContainer>
     </AuthContextProvider>
   );
