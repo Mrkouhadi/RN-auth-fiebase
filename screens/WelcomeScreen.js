4562145edc79b35939844e29419debf1d4e0593a
mrkouhadi <teacherbryan@outlook.com>
2022-04-13 01:25:03 +0800

    adding button of lohgout

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,10 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { useContext } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { AuthContext } from '../store/auth-context';
 
 function WelcomeScreen() {
+  const authCtx = useContext(AuthContext);
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Button title="Log out" color='red' onPress={()=>authCtx.logOut()}/>
     </View>
   );
 }
