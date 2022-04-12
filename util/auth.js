b94cfb562d92493b273f141528504bed29f84738
mrkouhadi <teacherbryan@outlook.com>
2022-04-13 01:24:54 +0800

    extracting the token

--- util/auth.js
+++ util/auth.js
@@ -10,12 +10,13 @@ const API_KEY = "AIzaSyAlOjfCkrGF8NBbtr2xSGuEexUpWkSrMcw"
                         password, 
                         returnSecureToken:true
                     });
-    console.log(response.data);
+    const token = response.data.idToken;
+    return token;
 }
-export const createUser = async (email, password) =>{
-    await authenticate('signUp', email, password);
+export const createUser =  (email, password) =>{
+    return authenticate('signUp', email, password);
 }
 
-export const login = async (email, password) =>{
-    await authenticate('signInWithPassword', email, password);
+export const login =  (email, password) =>{
+    return authenticate('signInWithPassword', email, password);
 }
