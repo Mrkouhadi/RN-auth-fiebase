c64e5eaad1d99eabcf7627689a9f6953f5306993
mrkouhadi <teacherbryan@outlook.com>
2022-04-13 01:24:25 +0800

    adding context authenticating

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,16 +1,20 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import { Alert } from 'react-native';
 import AuthContent from '../components/Auth/AuthContent';
 import LoadingOverlay from '../components/ui/LoadingOverlay';
 import { login } from '../util/auth';
+import { AuthContext } from '../store/auth-context';
 
 function LoginScreen() {
     const [isLogin, setIsLogin] = useState(false);
+    const authCtx = useContext(AuthContext);
 
     const loginHandler = async ({email,password})=>{
         setIsLogin(true);
         try{
-          await login(email,password);
+          const token = await login(email,password);
+          authCtx.authenticate(token);
+
         }catch(error){
           Alert.alert("Loggin in Failed !","Could not log in you in. PLease double check your credentials !")
         }
